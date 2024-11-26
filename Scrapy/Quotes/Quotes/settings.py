1559ea0796c028631c316a6d943c394651f76cd2
lemdark <76167358+lemdark@users.noreply.github.com>
2024-11-26 19:11:57 +0200

    Update settings.py

--- Scrapy/Quotes/Quotes/settings.py
+++ Scrapy/Quotes/Quotes/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "Quotes.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
